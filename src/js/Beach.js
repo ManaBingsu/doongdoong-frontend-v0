dc6ea35432629abc32fa637a8c6421c6e0d37954
ManaBingsu <43133819+ManaBingsu@users.noreply.github.com>
2021-01-07 19:54:48 +0900

    feat : Clean all bottles

--- src/js/Beach.js
+++ src/js/Beach.js
@@ -1,19 +1,45 @@
-createBottle()
-createBottle()
-createBottle()
-
 var count = 0;
-var maxBottleNum = 6;
+var maxBottleNum = 8;
+var interval = 500;
+var wave = document.getElementsByClassName('wave');
 
 create = setInterval(function() {
     if (count < maxBottleNum)
         createBottle();
- }, 2000);
+ }, interval);
+
+ function initialize() {
+    var tempEvent = document.getElementsByClassName('tempEvent');
+    for(let i = 0; i < tempEvent.length; i++) {
+        tempEvent[i].addEventListener("click", function() {
+          console.log("Clicked index: " + i);
+          for(let j = 0; j < wave.length; j++) {
+              var style = wave[i].style;
+              style.animationPlayState = "running";
+            } 
+              setTimeout(cleanBottle, 500);
+        })
+    }
+
+    const animated = document.querySelector('.wave');
+
+    animated.addEventListener('animationend', () => {
+        var target = wave[0];
+        target.classList.remove('wave');
+        void target.offsetWidth;
+        target.classList.add('wave');
+        target.style.animationPlayState = "paused";
+    console.log('Animation ended');
+});
+ }
+
+ initialize();
 
  function createBottle() {
         count += 1;
 
-        var div = document.createElement('div');
+        var div = document.createElement('button')
+
         div.className = "bottle";
 
         var divWidth = document.getElementById('beach').offsetWidth;
@@ -22,8 +48,8 @@ create = setInterval(function() {
         console.log(divWidth + divHeight);
 
         div.style.position = 'absolute';
-        div.style.left = String(getRandomPos(0, divWidth)) + 'px';
-        div.style.top = String(getRandomPos(0, divHeight)) + 'px';
+        div.style.left = String((Math.random()*100)%100) + '%';
+        div.style.top = String((Math.random()*100)%100) + '%';
         div.style.zIndex = String(count);
 
         var image = document.createElement('img');
@@ -31,6 +57,20 @@ create = setInterval(function() {
         image.src = './image/letter.svg';
         div.appendChild(image);
         document.getElementById('beach').appendChild(div);
+}
+
+function deleteBottle(bottle) {
+    bottle.parentNode.removeChild(bottle);
+}
+
+function cleanBottle() {
+    var beach = document.getElementById('beach');
+    var child = beach.lastElementChild;  
+        while (child) { 
+            beach.removeChild(child); 
+            child = beach.lastElementChild; 
+            count -= 1;
+        } 
 }
 
 function getRandomPos(minRange, maxRange) {
